96bd7c452da600ade0543f1c03a4cce3c085a4f9
will123195 <will123195@gmail.com>
2017-03-04 16:44:59 -0700

    fix npm 3 support

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ module.exports = function (config) {
 
   // because npm > 3.x has flat node_modules
   var paths = __dirname.split('node_modules/')
-  if (paths.length >= 3) {
-    root = [paths[0], paths[paths.length - 1]].join('node_modules/')
+  if (paths.length >= 2) {
+    root = paths[0]
   }
 
   console.log('[express-legal] root:', root)
