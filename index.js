c63dfef33be6c4435ffcc9d38762c7d2071b813a
will123195 <will123195@gmail.com>
2017-03-04 16:34:57 -0700

    support for npm 3+

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ var bleh = require('bleh')
 module.exports = function (config) {
   var app = express()
   config.COMPANYNAME = config.companyName.toUpperCase()
+  var root = __dirname
+
+  // because npm > 3.x has flat node_modules
+  var folders = __dirname.split('node_modules/')
+  if (folders.length >= 3) {
+    root = [folders[0], folders[folders.length - 1]].join('node_modules/')
+  }
+
   app.use(bleh({
     helpers: config,
-    root: __dirname,
+    root: root,
     dist: 'public/dist/legal'
   }))
   return app
