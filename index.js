a8854730176f2045b2a335ff8199b6eac9bc3292
will123195 <will123195@gmail.com>
2017-03-04 16:41:29 -0700

    debug

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ var bleh = require('bleh')
 module.exports = function (config) {
   var app = express()
   config.COMPANYNAME = config.companyName.toUpperCase()
+
   var root = __dirname
 
   // because npm > 3.x has flat node_modules
-  var folders = __dirname.split('node_modules/')
-  if (folders.length >= 3) {
-    root = [folders[0], folders[folders.length - 1]].join('node_modules/')
+  var paths = __dirname.split('node_modules/')
+  if (paths.length >= 3) {
+    root = [paths[0], paths[paths.length - 1]].join('node_modules/')
   }
+
+  console.log('[express-legal] root:', root)
 
   app.use(bleh({
     helpers: config,
